2f32d8938c3c6afe25e6d5d326ececf77a0d89ca
jiformblitz <j.illi@formblitz.de>
2014-11-12 09:36:12 +0100

    First Update
    Testing setup

---
+++ proyecto2/htdocs/admin/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+  <h1>Projecto 2 - Admin-Area</h1>
+  <p>
+    <? echo ('The Boss ' . time('d / m / Y H:i:s')); ?>
+  </p>
+</bodx>
+</html>
---
+++ proyecto2/htdocs/admin/secret.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+</head>
+<body>
+  <h1>Projecto 2 - Secret-Area</h1>
+  <p>
+    <? echo ('53kr37 ' . time('d / m / Y H:i:s')); ?>
+  </p>
+</bodx>
+</html>
